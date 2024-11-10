4043e59f2bce7570d2af37a02db1ca1252973c88
Augustine <augustineosezua1@gmail.com>
2024-11-10 12:49:30 -0500

    stmyth

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser'); 
 const path = require('path');
-require('commons')
+
 
 const app = express();
 
