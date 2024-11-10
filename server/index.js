c776d99580377f0593cbefe2f8316317d16c1da1
Augustine <augustineosezua1@gmail.com>
2024-11-10 12:22:12 -0500

    smth

--- server/index.js
+++ server/index.js
@@ -5,10 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser'); 
 const path = require('path');
 
-
 const app = express();
-
-
 
 app.use(cors());
 app.use(bodyParser.json());
