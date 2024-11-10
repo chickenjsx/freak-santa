27471c5a69e6ad47e79341225911f37314c3d756
Augustine <augustineosezua1@gmail.com>
2024-11-10 16:02:41 -0500

    seaching

--- server/index.js
+++ server/index.js
@@ -16,6 +16,16 @@ app.get('/*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 });
 
+app.get('/index', (req, res) => {
+  db.query('SELECT * FROM freaksanta', (err, results) => {
+      if (err) {
+          res.status(500).json({ error: err });
+      } else {
+          res.json(results);
+      }
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,"0.0.0.0", () => {
