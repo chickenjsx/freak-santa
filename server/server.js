714f29f11a3695c969449cb041326fa18afa5ac1
Augustine <augustineosezua1@gmail.com>
2024-11-09 22:36:04 -0500

    perchance a fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+
 
 const app = express();
 
@@ -45,7 +45,7 @@ app.post('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ||5432;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
